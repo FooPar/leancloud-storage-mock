d4b7875994f556f8a1e08ef76f68a98b80f35de9
Yushi Sun <sunyushi9546@gmail.com>
2017-02-20 20:48:58 +0800

    move the deepCompare function into here

--- leancloud-storage.js
+++ leancloud-storage.js
@@ -10,6 +10,40 @@ AV.Object.prototype.save = jest.fn(() => {
 });
 AV.Query.prototype.find = jest.fn(function() {
   console.log(`AV.Query.find called with ${JSON.stringify(this)} ${JSON.stringify(arguments)}`);
-  return AV.Promise.resolve("yoooo");
+  return AV.Promise.resolve("yooooo");
 });
 export default AV;
+
+// compare two av objects a and b, and log any discrepancies recursively to
+// provided logger.
+export function deepCompare(a, b, logger) {
+  if (!logger) logger = console.error;
+  if (!(typeof a === typeof b)) {
+    logger(`type different: ${a}, ${b}`);
+    return false;
+  }
+  if (typeof a === 'object') {
+    for (let key of Object.keys(a)) {
+      if (!(key in b)) {
+        logger(`key ${key} not in ${JSON.stringify(b)}`);
+        return false;
+      }
+      //NOTE: ignoring cid for av objects
+      if (key === 'cid') {
+        //ignore
+        return true;
+      }
+      if (!deepCompare(a[key], b[key])) {
+        logger(`objects ${JSON.stringify(a)}, ${JSON.stringify(b)} don't agree on ${key}`);
+        return false;
+      }
+    }
+    return true;
+  }
+  if (a === b) {
+    return true;
+  } else {
+    logger(`values ${a}, ${b} don't match`);
+    return false;
+  };
+}
